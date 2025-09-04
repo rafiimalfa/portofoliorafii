7b2732f23ef170872395ac8464a22bcaafca9d9b
rafiimalfar <rafii.razaqa@visiprima.id>
2025-09-04 10:02:47 +0700

    fix: include _next folder in GitHub Pages

---
+++ docs/_next/static/eLrudiWMCIjzZOA6lcMF-/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/adac5ff0fcf3b360.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
